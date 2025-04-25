5467355bc1da7c718e34f09c0682e18ed5fcfd39
Rayssa <andrade.rayssa.da.silva@gmail.com>
2025-04-25 14:04:03 -0300

    Imc2 atividade
    atividade no pycharm :)

---
+++ main.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/calcular_imc', methods=['POST'])
+def calcular_imc():
+    altura = float(request.form['altura'])
+    peso = float(request.form['peso'])
+
+    imc = round(peso / (altura ** 2), 2)
+
+    if imc < 18.5:
+        diagnostico = 'Abaixo do peso'
+
+    elif imc < 24.9:
+        diagnostico = 'Peso Normal'
+
+    elif imc < 29.9:
+        diagnostico = 'Acima do peso'
+
+    else:
+        diagnóstico = 'Obeso'
+
+    return render_template(template_name_or_list= 'index.html', imc=imc, diagnostico=diagnostico)
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
